23f35c492b55f44c92b864233bc052635d2b0b54
creanga <creanga@adobe.com>
2021-06-24 18:40:05 +0300

    struggling

--- src/main/scala/com/creanga/playground/spark/example/metrics/MetricListener.scala
+++ src/main/scala/com/creanga/playground/spark/example/metrics/MetricListener.scala
@@ -1,21 +1,38 @@
 package com.creanga.playground.spark.example.metrics
 
+import java.util.concurrent.atomic.AtomicLong
+
+import scala.collection.mutable
+
+import com.creanga.playground.spark.example.metrics.MetricListener.JOB_CONTEXT_NAME
 import org.apache.spark.scheduler.{SparkListener, SparkListenerStageSubmitted, SparkListenerTaskEnd}
 
-class MetricListener(name:String) extends SparkListener{
+class MetricListener(jobContextValue: String) extends SparkListener {
 
-  var rows: Long = 0L
-  var stageId = 0
+  var rowsRead: AtomicLong = new AtomicLong
+  var bytesRead: AtomicLong = new AtomicLong
+  var rowsWritten: AtomicLong = new AtomicLong
+  var bytesWritten: AtomicLong = new AtomicLong
+  var stageIds = new mutable.HashSet[Int]()
 
   override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted): Unit = {
-    if (stageSubmitted.properties.getProperty("job-context") == name){
-      stageId = stageSubmitted.stageInfo.stageId
+    if (stageSubmitted.properties.getProperty(JOB_CONTEXT_NAME) == jobContextValue) {
+      stageIds.add(stageSubmitted.stageInfo.stageId)
     }
   }
 
   override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
-    if (taskEnd.stageId == stageId)
-      rows = rows + taskEnd.taskMetrics.inputMetrics.recordsRead
+    if (stageIds.contains(taskEnd.stageId)) {
+      if (taskEnd.taskMetrics != null) {//taskMetrics is null for a failed task
+        rowsRead.addAndGet(taskEnd.taskMetrics.inputMetrics.recordsRead)
+        bytesRead.addAndGet(taskEnd.taskMetrics.inputMetrics.bytesRead)
+        rowsWritten.addAndGet(taskEnd.taskMetrics.outputMetrics.recordsWritten)
+        bytesWritten.addAndGet(taskEnd.taskMetrics.outputMetrics.bytesWritten)
+      }
+    }
   }
+}
 
+object MetricListener {
+  val JOB_CONTEXT_NAME = "job-context"
 }
--- src/main/scala/com/creanga/playground/spark/example/metrics/SparkMeasureMultiThreadedJobs.scala
+++ src/main/scala/com/creanga/playground/spark/example/metrics/SparkMeasureMultiThreadedJobs.scala
@@ -6,7 +6,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
-import org.apache.spark.scheduler.{SparkListener, SparkListenerStageSubmitted, SparkListenerTaskEnd}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -14,7 +13,7 @@ object SparkMeasureMultiThreadedJobs {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-        .setMaster("local[4]")
+        .setMaster("local[16]")
         .setAppName("StreamingTransactionExample")
         .set("spark.scheduler.mode", "FAIR")
     val sc = new SparkContext(conf)
@@ -23,31 +22,31 @@ object SparkMeasureMultiThreadedJobs {
     val first = Future({
       sc.setLocalProperty("spark.scheduler.pool", "fair")
       sc.setLocalProperty("job-context", "first")
+      val df = spark.read.load("/home/cornel/data.parquet")
       val listener = new MetricListener("first")
       sc.addSparkListener(listener)
-      val df = spark.read.load("/home/cornel/data.parquet")
 
       val countResult = df.filter(df("_verizon.emsLessDns") === "Y").count()
       println(countResult)
-      println(listener.rows)
+      println(listener.rowsRead.get())
       sc.removeSparkListener(listener)
     })
 
     val second = Future({
       sc.setLocalProperty("spark.scheduler.pool", "fair")
       sc.setLocalProperty("job-context", "second")
-      val listener = new MetricListener("second")
-      sc.addSparkListener(listener)
       val df = spark.read.load("/home/cornel/data.parquet")
 
-      val countResult = df.filter(df("_verizon.emsLessDns") === "Y").count()
+      val listener = new MetricListener("second")
+      sc.addSparkListener(listener)
+      val countResult = df.filter(df("_verizon.emsLessDns") =!= "Y").count()
       println(countResult)
-      println(listener.rows)
+      println(listener.rowsRead.get())
       sc.removeSparkListener(listener)
     })
 
-    Await.result(first, Duration(60, TimeUnit.SECONDS))
-    Await.result(second, Duration(60, TimeUnit.SECONDS))
+    Await.result(first, Duration(600, TimeUnit.SECONDS))
+    Await.result(second, Duration(600, TimeUnit.SECONDS))
 
   }
 
