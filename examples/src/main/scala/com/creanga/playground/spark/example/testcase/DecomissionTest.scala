7d62e3bc240b7a671e9509b04db928821eafe851
Corneliu Creanga <corneliu.creanga@crowdstrike.com>
2023-05-29 12:54:16 +0300

    wip

--- examples/src/main/scala/com/creanga/playground/spark/example/testcase/DecomissionTest.scala
+++ examples/src/main/scala/com/creanga/playground/spark/example/testcase/DecomissionTest.scala
@@ -9,18 +9,26 @@ object DecomissionTest {
 
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf();
+    val conf = new SparkConf()
     val sc = new SparkContext(conf)
     val jsc = new JavaSparkContext(sc)
 
-
     while (true) {
-      val taskTime: Long = (10 + Math.random * 40).toLong * 1000
-      jsc.parallelize(new util.ArrayList[AnyRef]).foreachPartition((_: util.Iterator[AnyRef]) => {
-          System.err.println(TaskContext.getPartitionId + " If you’re feeling bad, you just look at the cats, you’ll feel better, because they know that everything is, just as it is. There’s nothing to get excited about. They just know. They’re saviours. I'll wait " + taskTime + " ms")
-          Thread.sleep(taskTime)
-      })
-      Thread.sleep(taskTime + taskTime/2)
+      for (i <- 1 to 2) {
+        val thread: Thread = new Thread {
+          override def run(): Unit = {
+            val local = i;
+            val taskTime: Long = (10 + Math.random * 20).toLong * 1000
+            jsc.parallelize(new util.ArrayList[AnyRef]).foreachPartition((_: util.Iterator[AnyRef]) => {
+              System.err.println(local + " If you’re feeling bad, you just look at the cats, you’ll feel better, because they know that everything is, just as it is. There’s nothing to get excited about. They just know. They’re saviours. I'll wait " + taskTime + " ms")
+              Thread.sleep(taskTime)
+            })
+          }
+        }
+        thread.start()
+      }
+
+      Thread.sleep(60000)
     }
   }
 
