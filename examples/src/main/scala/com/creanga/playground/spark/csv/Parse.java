9ddf1b42f0fdaaf0a6751883c268d5e7ed04337f
cornelcreanga <cornelcreanga@yahoo.com>
2023-11-15 18:16:37 +0200

    wip/wip

---
+++ examples/src/main/scala/com/creanga/playground/spark/csv/Parse.java
@@ -0,0 +1,12 @@
+package com.creanga.playground.spark.csv;
+import com.mapzen.jpostal.AddressParser;
+
+public class Parse {
+//https://github.com/openvenues/jpostal
+//https://github.com/openvenues/libpostal
+//https://stackoverflow.com/questions/32838245/how-to-extract-all-the-address-information-from-text
+    public static void main(String[] args) {
+        AddressParser p = AddressParser.getInstance();
+    }
+
+}
